025c4f9e1eeffa5605e6a5d217413148013d4346
Colin Griffith <tynach2@gmail.com>
2014-03-31 23:01:01 -0700

    Following commenting guidelines.

--- public/forPages.php
+++ public/forPages.php
@@ -1,4 +1,9 @@
 <?php
 
+/******************************************************************************
+ * These files are necessary for any page that actually displays something in *
+ * the browser window.                                                        *
+ ******************************************************************************/
+
 include_once('necessary.php');
 include_once(sprintf('%s/%s', Location::ROOT(), 'page.php'));
