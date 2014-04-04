13d3408dbc9d13427e3c77d4990ed96fc03b8712
Colin Griffith <tynach2@gmail.com>
2014-04-04 00:25:34 -0700

    Re-renamed index.php, added new CSS and template.

---
+++ public/styles/main.css.php
@@ -0,0 +1,20 @@
+/* This will ensure editors color code this document correctly: <style> */
+<?php
+
+// Tell browsers this is a CSS document
+header("Content-type: text/css; charset: UTF-8");
+
+// Variables to use in the CSS
+$navWidth
+
+?>
+
+/*****************
+ * Global Styles *
+ *****************/
+
+* {
+	margin: 0;
+	padding: 0;
+	line-height: 1;
+}
